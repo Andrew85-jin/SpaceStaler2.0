d9b49384c1c1f159ac006a944e32a8b55f65fb7b
Andrew85-jin <andrejvizitiu857@gmail.com>
2026-01-22 21:29:58 +0200

    new controller, service and module for admin

--- ssxr_backend/src/app.module.ts
+++ ssxr_backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminController } from './admin/admin.controller';
+import { AdminService } from './admin/admin.service';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdminModule],
+  controllers: [AppController, AdminController],
+  providers: [AppService, AdminService],
 })
 export class AppModule {}
