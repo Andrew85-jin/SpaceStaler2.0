6b0a138fe9aa9d6729a4ce645215267b64b12178
Andrew85-jin <andrejvizitiu857@gmail.com>
2026-01-22 21:51:21 +0200

    connect DB

--- ssxr_backend/src/admin/admin.module.ts
+++ ssxr_backend/src/admin/admin.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin } from './admin.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Admin])],
+    providers: [AdminService],
+    controllers: [AdminController]
+})
 export class AdminModule {}
--- ssxr_backend/src/app.module.ts
+++ ssxr_backend/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
 import { AdminModule } from './admin/admin.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AdminModule],
+  imports: [
+      AdminModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'SpaceStaler',
+        autoLoadEntities: true,
+        synchronize: true,
+      })
+  ],
   controllers: [AppController, AdminController],
   providers: [AppService, AdminService],
 })
